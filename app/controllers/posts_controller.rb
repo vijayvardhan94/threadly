39d913f4c10a25c4f95f1d9f222f341d1571e3cc
vijayvardhan94 <tadimetv@oregonstate.edu>
2021-07-20 14:42:23 -0700

    updating controllers and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
     def index
+        @new_post = Post.new
+        @all_posts = Post.order(created_at: :desc).all
     end
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:comment)
+    end
+
 end
